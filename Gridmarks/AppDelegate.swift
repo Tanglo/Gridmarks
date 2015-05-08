663d096fa7e32a04b61ae5da80efc37643a8030c
Tanglo <Tanglo@users.noreply.github.com>
2015-05-08 14:16:00 +1000

    Initial Commit

---
+++ Gridmarks/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Gridmarks
+//
+//  Created by Lee Walsh on 8/05/2015.
+//  Copyright (c) 2015 Lee David Walsh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GridmarksTests/GridmarksTests.swift
@@ -0,0 +1,36 @@
+//
+//  GridmarksTests.swift
+//  GridmarksTests
+//
+//  Created by Lee Walsh on 8/05/2015.
+//  Copyright (c) 2015 Lee David Walsh. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class GridmarksTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
