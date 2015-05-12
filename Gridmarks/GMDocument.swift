4aef8b7496fa3b8d65776e01d6f206e6d62fb2be
Lee Walsh <l.walsh@neura.edu.au>
2015-05-12 16:25:43 +1000

    Writes the GMDocument to a .gridmarks file.
    Updates experimentWindow fields when opening an existing document.
    Starts the camera that is selected.

--- Gridmarks/GMDocument.swift
+++ Gridmarks/GMDocument.swift
@@ -10,7 +10,7 @@ import Cocoa
 import LabBot
 
 class GMDocument: DRHExperimentDocument {
-    let experimentData = DRHExperimentData()
+    var experimentData = DRHExperimentData()
     var camera = DRHStillCamera()
 
     override init() {
@@ -35,16 +35,21 @@ class GMDocument: DRHExperimentDocument {
     override func dataOfType(typeName: String, error outError: NSErrorPointer) -> NSData? {
         // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
         // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-        outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
-        return nil
+ //       outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+ //       return nil
+        return NSKeyedArchiver.archivedDataWithRootObject(experimentData)
     }
 
     override func readFromData(data: NSData, ofType typeName: String, error outError: NSErrorPointer) -> Bool {
         // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
         // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
         // If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-        outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
-        return false
+//        outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+//        return false
+        let newExperimentData = NSKeyedUnarchiver.unarchiveObjectWithData(data) as! DRHExperimentData
+        
+        experimentData = newExperimentData
+        return true
     }
     
 }
--- Gridmarks/GMExperimentWindowController.swift
+++ Gridmarks/GMExperimentWindowController.swift
@@ -26,6 +26,14 @@ class GMExperimentWindowController: DRHExperimenterWindowController {
             cameraPopup!.addItemWithTitle(currentCamera.localizedName!!)
         }
         cameraPopup!.selectItem(nil)
+        experimentNameField().stringValue = (document! as! GMDocument).experimentData.experimentName
+        experimentSubjectField().stringValue = (document! as! GMDocument).experimentData.experimentSubject
+        experimentSessionField().stringValue = (document! as! GMDocument).experimentData.experimentSession
+        experimentDatePicker().dateValue = (document! as! GMDocument).experimentData.experimentDate
+        experimentFilenameStemField().stringValue = (document! as! GMDocument).experimentData.experimentFilenameStem
+        if (document! as! GMDocument).fileURL != nil {
+            saved = true
+        }
     }
     
     override func experimentIsFinished() -> Bool {
@@ -42,6 +50,7 @@ class GMExperimentWindowController: DRHExperimenterWindowController {
                 (document! as! GMDocument).experimentData.experimentSession = experimentSessionField().stringValue
                 (document! as! GMDocument).experimentData.experimentFilenameStem = experimentFilenameStemField().stringValue
                 (document! as! GMDocument).experimentData.experimentDate = experimentDatePicker().dateValue
+                initialiseExperiment()
                 let fileDate = DRHFileDate(date: (document! as! GMDocument).experimentData.experimentDate)
                 var fileName = "\((document! as! GMDocument).experimentData.experimentFilenameStem)_"
                 fileName += "\((document! as! GMDocument).experimentData.experimentSubject)"
@@ -53,6 +62,18 @@ class GMExperimentWindowController: DRHExperimenterWindowController {
                 savePanel.extensionHidden = false
                 let result = savePanel.runModal()
                 if result == NSModalResponseOK {
+                    //save the gridmarks document
+                    println("\(savePanel.URL!.path!)")
+                    let saveURL = NSURL(fileURLWithPath: savePanel.URL!.path! + ".gridmarks")!
+                    println("\(saveURL)")
+                    (document! as! GMDocument).saveToURL(saveURL, ofType: "com.LDW.gridmarks", forSaveOperation: NSSaveOperationType.SaveAsOperation, completionHandler: {(error:NSError!) -> Void in
+                        if error != nil {
+                            println("\(error.description)")
+                        } else {
+                            println("Saved")
+                        }
+                        })
+                    
                     //write the experimentData as text
                     let savePath = savePanel.URL!.path! + ".txt"
                     (document! as! GMDocument).experimentData.writeExperimentDataTo(savePath)
@@ -77,6 +98,7 @@ class GMExperimentWindowController: DRHExperimenterWindowController {
             startButton!.enabled = false
             finishButton!.enabled = false
             stopButton!.enabled = true
+            
             let newSubjWindowController = GMSubjectWindowController(screenNumber: 1, andFullScreen: true)
             (document! as! GMDocument).subjectWindowController = newSubjWindowController
             newSubjWindowController.showWindow(self)
@@ -105,5 +127,29 @@ class GMExperimentWindowController: DRHExperimenterWindowController {
         finishedExperiment = true
     }
     
+    @IBAction func startCamera(sender: AnyObject){
+        (document! as! GMDocument).camera.selectDeviceAtIndex(sender.indexOfSelectedItem())
+        if !(document! as! GMDocument).camera.startCameraSession() {
+            let alert = NSAlert()
+            alert.messageText = "Could not start the camera"
+            alert.addButtonWithTitle("Bugger")
+            alert.informativeText = "Something is wrong with the camera, it couldn't be started"
+            alert.runModal()
+        }
+    }
+    
+    func initialiseExperiment(){
+        var baseTrials: [[AnyObject]] = [[0,0],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[0,8],[0,9],[0,10]]
+        baseTrials += [[1,0],[1,1],[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8],[1,9],[1,10]] as [[AnyObject]]
+        var trialMatrix = [[AnyObject]]()
+        for i in 0..<5 {
+            trialMatrix += baseTrials
+        }
+        let dataMatrix = LBDataMatrix(variableNames: ["condition","landmark"], observations: trialMatrix)
+        var responseArray = Array<AnyObject>(count: 110, repeatedValue: Double.NaN)
+        dataMatrix.appendVariable("response", values: responseArray)
+        dataMatrix.shuffleObservations()
+        (document! as! GMDocument).experimentData.experimentDataMatrix = dataMatrix
+    }
 }
 
